4779d547884afc605fd508fe6232efcd25fa927b
k-ing-ing <1762507696@qq.com>
2025-09-12 10:33:09 +0800

    完成“重量转换程序”作业

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if unit == "kg":
      print(f"对应的英制重量为{result:.3f}磅")
 elif unit == "pd":
      result = weight / 2.2046
-     print(f"对应的公制重量为{result:.3f}公斤")
+     print(f"对应的公制重量为{result:.2f}公斤")
 else:
      print("输入格式错误，请使用'xxkg'或'xxpd'格式（如10kg、10pd）")
